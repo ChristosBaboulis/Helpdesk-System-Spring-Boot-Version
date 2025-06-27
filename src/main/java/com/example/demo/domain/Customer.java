822d7c7513bc75713a07996a8d697f19a005e67a
ChristosBaboulis <chrisb2603@gmail.com>
2025-06-27 22:09:01 +0300

    Added Customer class on domain model and relevant test class

---
+++ src/main/java/com/example/demo/domain/Customer.java
@@ -0,0 +1,53 @@
+package com.example.demo.domain;
+
+import com.example.demo.contacts.Address;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "customer_code", length = 20, nullable = false)
+    private String customerCode;
+
+    @Embedded
+    private PersonalInfo personalInfo;
+
+    public Customer() {}
+
+    //Constructor with address as argument
+    public Customer(String customerCode, String firstName, String lastName,
+                    String telephoneNumber, String emailAddress,
+                    LocalDate birthdate, Address address) {
+        this.customerCode = customerCode;
+        this.personalInfo = new PersonalInfo(firstName, lastName, emailAddress, telephoneNumber, birthdate, address);
+    }
+
+    // GETTERS - SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+}
