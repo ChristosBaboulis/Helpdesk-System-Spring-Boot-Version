414eec214da796a81e3c40c4ceedb46ec41bd85a
ChristosBaboulis <chrisb2603@gmail.com>
2025-06-30 20:57:48 +0300

    Created CustomerRepresentation class

---
+++ src/main/java/com/example/demo/representation/CustomerRepresentation.java
@@ -0,0 +1,21 @@
+package com.example.demo.representation;
+
+import com.example.demo.contacts.Address;
+import org.springframework.aot.hint.annotation.RegisterReflection;
+
+import java.time.LocalDate;
+
+@RegisterReflection
+public class CustomerRepresentation {
+    public int Id;
+    public String customerCode;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phone;
+    public LocalDate birthdate;
+    public Address address;
+
+    //No-args constructor for JSON Deserialization
+    public CustomerRepresentation() {}
+}
